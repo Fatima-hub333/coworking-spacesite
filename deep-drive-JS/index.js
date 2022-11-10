90b3e954400c546c46cbcfc9ea97ef0d63002140
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-11-10 13:46:42 +0500

    update files

---
+++ deep-drive-JS/index.js
@@ -0,0 +1,16 @@
+let userName = 'Jane Doe';
+let message = `Hi ${userName}, how are you?`;
+console.log(message);
+
+// better way to concatenate strings which is called 'template literal'
+
+const weight = 150;
+const moonWeight = `You weigh ${weight} pounds on the moon`;
+console.log(moonWeight)
+
+const practice = 20;
+const sentence = `I practice this ${practice} times at the evening`;
+console.log(sentence);
+
+const threeLines = "This is a string \nthat spans \nthree lines."
+console.log(threeLines)
