9ed56a354e686799a5089776259a56d5d5877906
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-11-12 12:44:58 +0500

    Solve challenge

--- deep-drive-JS/12-Nov.js
+++ deep-drive-JS/12-Nov.js
@@ -1,6 +1,6 @@
 // {Even Shorter Conditionals with short-Circuiting}
-const response = "Read";
-const isEmailVerified = true;
+// const response = "Read";
+// const isEmailVerified = true;
 // let userName;
 
 // if (response) {
@@ -20,5 +20,23 @@ const isEmailVerified = true;
 // const result = true && false;
 // console.log(result);
 
-const userName = response && isEmailVerified || "guest";
-console.log(userName);
+// const userName = response && isEmailVerified || "guest";
+// console.log(userName);
+
+// {Short-circuiting Challenge}
+
+const Karma = 143;
+const isModerator = true;
+
+let hasEnoughKarma = Karma > 100 ? true : false;
+let canUpvote = isModerator || hasEnoughKarma;
+console.log("canUpvote:", canUpvote);
+
+
+let canDelete = hasEnoughKarma && isModerator;
+console.log("canDelete:", canDelete);
+
+const response = "JohnDoe";
+const hasValidEmail = true;
+const user = hasValidEmail && (response || "guest");
+console.log("user: ", user);
