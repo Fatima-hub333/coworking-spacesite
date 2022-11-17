c284f13263b23585bfa800d6abf99847d8f699f3
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-11-17 10:59:41 +0500

    challenge - Object destructuring

--- deep-drive-JS/17th-Nov.js
+++ deep-drive-JS/17th-Nov.js
@@ -41,3 +41,25 @@ displayUserBio(user)
 // }
 
 // displayUser()
+
+// [Challenge; Object Destructuring]
+const recommendations = {
+  pancakes: 'Nowhere Man',
+  riceBowls: 'Pompoko',
+  beer: 'The Craft Beer Co.',
+  coffee: 'Coffee Roasters',
+  small_plates: 'VeneTian Plates',
+  music: {
+    traditional: 'Fiddler\'s Elbow',
+    jazz: 'The Paris House'
+  }
+}
+
+const { beer, coffee } = recommendations;
+const { music: { traditional, jazz } } = recommendations;
+
+function displayMusicPlaces({ music: { traditional, jazz } }) {
+  console.log(`Head to ${traditional} or ${jazz} to listen to great music!`)
+}
+
+displayMusicPlaces(recommendations);
