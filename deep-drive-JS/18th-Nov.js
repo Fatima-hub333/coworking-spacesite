4316265f3fb3553c2c00697e7602a837f4ef8bfe
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-11-18 10:38:36 +0500

    How Maps can Do What Objects Can't

--- deep-drive-JS/18th-Nov.js
+++ deep-drive-JS/18th-Nov.js
@@ -15,4 +15,48 @@ const newUser = {
 const createdUser = { ...user, ...newUser, verfified: false, name: 'Fatima', age: '25', phoneNumber: '03477871262'};
 console.log(createdUser);
 
+// [How Maps can Do What Objects Can't]
+const nums = {
+  1: 1,
+  true: true
+};
 
+// const map1 = new Map([
+//   [1, 1],
+//   [true, true]
+// ]);
+
+// map1.set('key', 'value');
+
+// map1.forEach((value, key) => {
+//   console.log(key, value);
+// });
+// [...map1.keys()]
+
+const user1 = { name: 'John' }
+const user2 = { name: 'Fatima' }
+
+const secretKey1 = 'avhvvyj';
+const secretKey2 = 'bnngyiukjn';
+
+const secretKeyMap = new Map([
+  [user1, secretKey1],
+  [user2, secretKey2]
+]);
+
+const key = secretKeyMap.get(user2);
+console.log(key)
+
+const favouritePlace = {
+  music: 'jazz',
+  name: 'Paris House'
+}
+
+const favouritePlaceMap = new Map([
+  ['music', 'jazz'],
+  ['name', 'Paris House'],
+  ['visited', true],
+  ['averageBill', 17.4]
+]);
+
+console.log(favouritePlace.get('averageBill'));
