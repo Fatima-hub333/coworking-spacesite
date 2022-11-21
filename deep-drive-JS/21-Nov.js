52bf5b1e3840687db52129d69285a90bec354767
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-11-21 12:05:25 +0500

    build flexible collections with Arrays

---
+++ deep-drive-JS/21-Nov.js
@@ -0,0 +1,15 @@
+const todos = {};
+
+const todo1 = {
+  text: 'Wash the dishes',
+  complete: false
+};
+
+const todo2 = {
+  text: 'Eat Lunch',
+  complete: false
+};
+
+todos[1] = todo1;
+todos[2] = todo2;
+console.log(todos);
