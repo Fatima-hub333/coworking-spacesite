2491fcefcfa0dce2ec4db9c60cf4ad7a85b82564
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-11-21 12:14:55 +0500

    solve challenge

--- deep-drive-JS/21-Nov.js
+++ deep-drive-JS/21-Nov.js
@@ -1,4 +1,4 @@
-const todos = {};
+const todos = [];
 
 const todo1 = {
   text: 'Wash the dishes',
@@ -10,6 +10,20 @@ const todo2 = {
   complete: false
 };
 
-todos[1] = todo1;
-todos[2] = todo2;
+todos.push(todo1, todo2);
+todos.pop()
 console.log(todos);
+
+// [First Array challenge]
+// 1. Create an array - favouriteSongs[] -  and add your 3 favourite songs using push()
+// 2. Log out the last song in the array, and make it so that your solution will log out the last one regardless of how many items there are in the array
+// 3. Invoke the pop method on your created array. What values do you have left in the array? 
+const favouriteSongs = [];
+const song1 = 'abcdef'
+const song2 = 'ghijk'
+const song3 = 'lmnoqr'
+
+favouriteSongs.push(song1, song2, song3);
+console.log(favouriteSongs[favouriteSongs.length - 1])
+favouriteSongs.pop()
+console.log(favouriteSongs[favouriteSongs.length - 1]);
