6eadaebbc75c34831aac289c0d2c94890d3e2e33
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-11-22 10:23:05 +0500

    understand the power of reduce

--- deep-drive-JS/22-Nov.js
+++ deep-drive-JS/22-Nov.js
@@ -42,3 +42,44 @@ const total = menuItems.reduce((accumulator, menuItem) => {
 }, 0);
 
 console.log(total);
+
+// {Transform Arrays Challenge}
+const cars = [
+  { name: "Toyota", isElectric: false, weight: 1320 },
+  { name: "Ford", isElectric: false, weight: 1400 },
+  { name: "Volkswagen", isElectric: false, weight: 1370 },
+  { name: "Honda", isElectric: false, weight: 1375 },
+  { name: "Tesla", isElectric: true, weight: 1750 },
+  { name: "BMW", isElectric: true, weight: 1350 },
+];
+
+const totalWeight = cars.reduce((accumulator, car) => {
+  if (car.isElectric) {
+    return accumulator + car.weight;
+  } else {
+    return accumulator;
+  }
+}, 0)
+console.log(totalWeight);
+
+
+// [Understand the power of .reduce()]
+const numbers = [1, 2, 3, 4, 5, 6];
+
+// const doubledNumbers = numbers.map(num => num * 2);
+// const doubledNumbers = numbers.reduce((acc, num) => {
+//   acc.push(num * 2);
+// }, []);
+// console.log('Doubled numbers', doubledNumbers);
+// console.log('numbers', numbers);
+
+// const greaterNumbers = numbers.reduce((acc, num) => {
+//   if (num > 3) {
+//     acc.push(num);
+//   }
+//   return acc;
+// }, []);
+
+// const greaterNumbers = numbers.filter(num => num > 3);
+const greaterNumbers = numbers.reduce((acc, num) => num > 3 ? acc.concat(num) : acc, []);
+console.log(greaterNumbers);
