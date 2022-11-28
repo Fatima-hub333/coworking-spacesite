11f2aef997fa7eb2e65300dfe484f917bb41a7b8
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-11-28 09:13:54 +0500

    gow to get & set  and simplify classes

--- deep-drive-JS/28-Nov.js
+++ deep-drive-JS/28-Nov.js
@@ -52,7 +52,34 @@ class SaleProduct extends Product {
 const SaleProduct1 = new SaleProduct("Coffe Maker", 99, false, 20);
 console.log(SaleProduct1.getSalePrice())
 
+// How to Get, Set and Simplify Classes
+class Inventory {
+  constructor(name, price, discountable) {
+    this.name = name;
+    this._price = price;
+    this.discountable = discountable;
+  }
 
+  get price() {
+    return this._price;
+  }
+  
+  set price(price) {
+    if (typeof price !== "number") {
+      return this._price;
+    } else {
+      this._price = price; 
+    }
+  }
+}
+
+const product1 = new Inventory("Coffee Maker", 99.95, false);
+product1.price = 30;
+console.log(product1.price);
+// product1.price = {};
+// console.log(product1.clearancePrice);
+// product1.price = {};
+// console.log(product1.newPrice);
 
 
 
