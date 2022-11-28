e5b7c2af2af4bd6c66eafeff68ed6624c96f2519
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-11-28 08:51:51 +0500

    extends properties from classes

--- deep-drive-JS/28-Nov.js
+++ deep-drive-JS/28-Nov.js
@@ -22,3 +22,59 @@ console.log(myFavouriteFilm.director)
 myFavouriteFilm.addGenre("Thrilling");
 console.log(myFavouriteFilm.genres)
 console.log(myFavouriteFilm.getFilmTitle())
+
+// Share Class features with Extends
+class Product{
+  constructor(name, price, discountable) {
+    this.name = name;
+    this.price = price;
+    this.discountable = discountable;
+  }
+  isDiscountable() {
+    return this.discountable;
+  }
+}
+
+class SaleProduct extends Product {
+  constructor(name, price, discountable, percentOff) {
+    super(name, price, discountable);
+    this.percentOff = percentOff;
+  }
+  getSalePrice() {
+    if (super.isDiscountable()) {
+      return this.price * ((100 - this.percentOff) / 100)
+    } else {
+      return `${this.name} is not eligible for a discount`
+    }
+  }
+}
+
+const SaleProduct1 = new SaleProduct("Coffe Maker", 99, false, 20);
+console.log(SaleProduct1.getSalePrice())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
