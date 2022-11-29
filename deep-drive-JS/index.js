b10b4c21f8f3cc1273a7b11823731184a25523d5
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-11-29 10:06:26 +0500

    Get single & multiple elements

--- deep-drive-JS/index.js
+++ deep-drive-JS/index.js
@@ -1,7 +1,21 @@
-const p = document.createElement('p');
-p.innerText = 'Hello World';
-document.body.append(p)
+// const p = document.createElement('p');
+// p.innerText = 'Hello World';
+// document.body.append(p)
 
-document.body.style.background = "Yellow";
-p.style.color = "Red";
-p.addEventListener('click', () => console.log('clicked'))
+// document.body.style.background = "Yellow";
+// p.style.color = "Red";
+// p.addEventListener('click', () => console.log('clicked'))
+
+// const links = document.querySelectorAll('a');
+// console.log(link);
+
+// links.forEach(link => {
+//   if (link.matches('[href="/login"]')) {
+//     const loginLink = link;
+//     console.log(loginLink);
+//   }
+  
+// })
+
+const divs = document.getElementsByTagName('div');
+console.log(divs);
