d7ac912d505a443678e75a66da53c8190875d4dd
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-12-05 06:32:52 +0500

    create basic layout

--- deep-drive-JS/2-December.js
+++ deep-drive-JS/2-December.js
@@ -3,7 +3,7 @@
 //   .then(data => console.log(data));
 // const getBlogPost = async () => {}
 
-// async function getBlogPost() { 
+// async function getBlogPost() {
 //   const promise = new Promise((resolve, reject) => {
 //     setTimeout(() => resolve('blog post'), 1000)
 //   });
@@ -15,10 +15,42 @@
 
 // getBlogPost();
 
-async function getPost() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
-  const data = await response.json();
-  console.log(data);
+// async function getPost() {
+//   const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+//   const data = await response.json();
+//   console.log(data);
+// }
+
+// getPost();
+
+// async function runAsync() {
+//   try {
+//     await Promise.resolve('Hello World');
+//     null.someProperty = true;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// runAsync();
+
+// async function runAsync() {
+//   return await Promise.reject(Error('Oops'));
+// }
+// runAsync().catch(error => console.error(error));
+
+async function getGithubUser() {
+  try {
+    const response = await fetch('https://api.github.com/users/laksjdflasjfdlkjadfjk');
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    const person = await response.json();
+    console.log(person);
+  }
+  catch (error) {
+    console.error(error);
+  }
 }
 
-getPost();
+getGithubUser();
